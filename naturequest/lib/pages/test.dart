36e71af1f7a00da97eb08494054906424ff67a81
Jp <gonzalez.jean1503@gmail.com>
2025-11-08 15:40:40 -0600

    wokring on db crap

--- naturequest/lib/pages/test.dart
+++ naturequest/lib/pages/test.dart
@@ -2,98 +2,72 @@ import 'package:flutter/material.dart';
 import '/../main.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 
-class Testpage extends StatelessWidget
+
+class Testpage extends StatefulWidget
 {
   const Testpage({super.key});
 
   @override
-  Widget build(BuildContext context)
+  State<Testpage> createState() => _TestpageState();
+}
+  class _TestpageState extends State<Testpage>
   {
-    return Scaffold
-    (
-      backgroundColor: NatureQuestApp.softMintGreen,
-      body: Center
-      (
-        child: Column
+    String status = 'Waiting for NFC...';
+
+    Future<void> startNfcSession() async 
+    {
+      setState(() => status = 'Starting NFC session...');
+      NfcAvailability isAvailable = await NfcManager.instance.checkAvailability();
+
+      if (isAvailable == NfcAvailability.enabled)
+      {
+        setState(() => status = 'NFC session started. Waiting for target');
+        NfcManager.instance.startSession
         (
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:
-          [
-            Text(
-              'This is a test page',
-              style: TextStyle(
-                fontFamily: 'Merienda',
-                fontSize: 32,
-                fontWeight: FontWeight.w700,
-                color: NatureQuestApp.lsuGold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ]
+          pollingOptions: {NfcPollingOption.iso},
+          onDiscovered: (NfcTag tag) async 
+          {
+            try 
+            {
+              final msg = tag.data.toString();
+              setState(() => status = "msg: $msg");
+            }
+            catch(e)
+            {
+              setState(() => status = "Error");
+            }
+          }
+        );
+      }
+      else
+      {
+        setState(() => status = 'Still Waiting for Session to start.....');
+      }
+    }
+
+  
+  
+    @override
+    Widget build(BuildContext context)
+    {
+      return Scaffold
+      (
+        backgroundColor: NatureQuestApp.softMintGreen,
+        body: Center
+        (
+          child: Column
+          (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:
+            [
+              Text(status),
+              ElevatedButton(onPressed: startNfcSession, child: Text("Start Nfc"))
+            ]
+          )
         )
-      )
-    );
+      );
+    }
   }
-}
 
 
 
-
-
-class LandingPage extends StatelessWidget 
-{
-  const LandingPage({super.key});
-
-  @override
-  Widget build(BuildContext context) 
-  {
-    return Scaffold
-    (
-      backgroundColor: NatureQuestApp.softMintGreen,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Choose an Option',
-              style: TextStyle(
-                fontFamily: 'Merienda',
-                fontSize: 32,
-                fontWeight: FontWeight.w700,
-                color: NatureQuestApp.lsuGold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to login page
-              },
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 20),
-              ),
-              child: const Text
-              (
-                'Log In',
-                style: TextStyle(fontSize: 22),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to signup page
-              },
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 20),
-              ),
-              child: const Text(
-                'Sign Up',
-                style: TextStyle(fontSize: 22),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}

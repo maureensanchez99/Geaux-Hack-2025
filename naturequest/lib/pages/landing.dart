a01893ca75fb0a1167ccf1d9fed6b00c22b2ddd2
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-11-08 11:23:15 -0600

    saving updated

--- naturequest/lib/pages/landing.dart
+++ naturequest/lib/pages/landing.dart
@@ -7,7 +7,7 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: NatureQuestApp.softMintGreen,
+      backgroundColor: NatureQuestApp.softSunYellow,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -18,7 +18,14 @@ class LandingPage extends StatelessWidget {
                 fontFamily: 'Merienda',
                 fontSize: 32,
                 fontWeight: FontWeight.w700,
-                color: NatureQuestApp.lsuGold,
+                color: NatureQuestApp.natureGreen,
+                shadows: [
+                  Shadow(
+                    offset: const Offset(10.0, 5.0),
+                    blurRadius: 5.0,
+                    color: NatureQuestApp.earthyBrown.withOpacity(0.6),
+                  ),
+                ]
               ),
               textAlign: TextAlign.center,
             ),

aac95672d76dedb87dd360c8fd01a28a7e8ce2b0
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-11-09 00:59:53 -0600

    achievements page style

--- naturequest/lib/pages/achievements.dart
+++ naturequest/lib/pages/achievements.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
+import '/../main.dart';
 
 class AchievementPage extends StatelessWidget {
   const AchievementPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Achievements',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column( 
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start, 
+        children: [
+          Text(
+            'Achievements', 
+            style: TextStyle(
+              fontFamily: 'Merienda',
+              fontSize: 40,
+              fontWeight: FontWeight.w700,
+              color: NatureQuestApp.natureGreen,
+            )
+          ),
+          const SizedBox(height: 80),
+          Text(
+            'Challenges Completed:', 
+            // Pull challenge location from database
+            style: TextStyle(
+              fontFamily: 'Merienda',
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+              color: NatureQuestApp.natureGreen,
+            )
+          ),
+          const SizedBox(height: 40),
+          Text(
+            'Badges Collected:', 
+            // Pull challenge description from database
+            style: TextStyle(
+              fontFamily: 'Merienda',
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+              color: NatureQuestApp.natureGreen,
+            )
+          ),
+        ],
       ),
     );
   }

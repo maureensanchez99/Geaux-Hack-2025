6b850c0c9a8deef8e5a6924a4cb7a8dad364fcd2
Jp <gonzalez.jean1503@gmail.com>
2025-11-09 09:58:56 -0600

    login page done

--- naturequest/lib/pages/login.dart
+++ naturequest/lib/pages/login.dart
@@ -2,9 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'mainNavigation.dart';
 import '../main.dart'; // fixed relative import
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class LoginPage extends StatelessWidget {
+
+const baseURL = 'http://100.111.150.67:8000';
+
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState(); 
+}
+class _LoginPageState extends State<LoginPage>
+{
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _isLoading = false;
+  String? _errorMsg;
+
+  Future<void> _login() async {
+    setState(()
+    {
+      _isLoading = true;
+      _errorMsg = null;
+    }
+    );
+    final username = _usernameController.text.trim();
+    final password = _passwordController.text.trim();
+
+    try
+    {
+      final res = await http.post
+      (
+        Uri.parse('$baseURL/auth/login'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'username': username, 'password': password})
+      );
+    
+    if (res.statusCode == 200)
+    {
+      if (!mounted) return;
+      Navigator.pushReplacement( context, MaterialPageRoute(builder: (_) => const MainNavigation()));
+    }
+    else
+    {
+      setState(() => _errorMsg = 'Invalid username or password');
+    }
+    }
+    catch (e)
+    {
+      setState(() => _errorMsg = 'Connection error: $e');
+    }
+    finally
+    {
+      setState(() => _isLoading = false);
+    }
+    
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +84,7 @@ class LoginPage extends StatelessWidget {
               SizedBox(
                 width: 300,
                 child: TextFormField(
+                  controller: _usernameController,
                   decoration: const InputDecoration(
                     border: UnderlineInputBorder(),
                     labelText: 'Enter your username',
@@ -38,6 +97,7 @@ class LoginPage extends StatelessWidget {
                 width: 300,
                 child: TextFormField(
                   obscureText: true,
+                  controller: _passwordController,
                   decoration: const InputDecoration(
                     border: UnderlineInputBorder(),
                     labelText: 'Enter your password',
@@ -46,14 +106,14 @@ class LoginPage extends StatelessWidget {
               ),
               const SizedBox(height: 50),
 
+              if (_errorMsg != null)
+                Text(_errorMsg!, style: const TextStyle(color: Colors.red)),
+              if (_isLoading) const CircularProgressIndicator(),
+
+
               // Login button
               ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MainNavigation()),
-                  );
-                },
+                onPressed: _isLoading ? null : _login,
                 style: ElevatedButton.styleFrom(
                   backgroundColor: NatureQuestApp.softLavender,
                   padding:
